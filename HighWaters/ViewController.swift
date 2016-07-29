e121df222b5a9b507893c147ed53ac18da491c6a
Efrain <jesusayllonroa@gmail.com>
2016-07-28 22:45:05 -0500

    Gets lat long from current location

--- HighWaters/ViewController.swift
+++ HighWaters/ViewController.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import CloudKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDelegate {
+
+     var longitude: String!
+     var latitude: String!
+    @IBOutlet weak var mapView :MKMapView!
+    
+    var container :CKContainer!
+    var publicDB :CKDatabase!
+    var privateDB :CKDatabase!
+    
+    var locationManager  :CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationSetup()
+        self.container = CKContainer.defaultContainer()
+        self.publicDB = self.container.publicCloudDatabase
+        self.privateDB = self.container.publicCloudDatabase
+    
     }
+    
+    private func locationSetup(){
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.mapView.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+    }
+    
+    
+    
+    func mapView(mapView: MKMapView, didAddAnnotationViews views: [MKAnnotationView]) {
+        if let annotationView = views.first {
+            if let annotation = annotationView.annotation {
+                if annotation is MKUserLocation {
+                    let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 250, 250)
+                    self.mapView.setRegion(region, animated: true)
+                }
+            }
+        }
+        
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
